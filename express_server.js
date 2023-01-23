c9dac05b653a5d5bb2b108ce0846bb8fe968c57d
Leonardo Picchi Dancona <lpdancona@gmail.com>
2023-01-23 15:05:14 -0800

    adding links looping through table

--- express_server.js
+++ express_server.js
@@ -1,12 +1,12 @@
 const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
+app.set("view engine", "ejs");
 
 const urlDatabase = {
   b2xVn2: "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com",
 };
-
 app.get("/", (req, res) => {
   res.send("Hello!");
 });
@@ -19,4 +19,8 @@ app.get("/urls.json", (req, res) => {
 });
 app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
+});
+app.get("/urls", (req, res) => {
+  const templateVars = { urls: urlDatabase };
+  res.render("urls_index", templateVars);
 });
